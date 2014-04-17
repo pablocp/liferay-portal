3639477b1ab6f4e4d42a41cd65bf680283439807
Pablo Pinheiro <pablo.carvalho@liferay.com>
2014-04-17 18:16:02 -0300

    Partial implementation of the forms to json converters

---
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/storage/FormToJSONConverter.java
@@ -0,0 +1,61 @@
+package com.liferay.portlet.dynamicdatamapping.storage;
+
+import com.liferay.portlet.dynamicdatamapping.layout.FormField;
+import com.liferay.portlet.dynamicdatamapping.layout.Form;
+import com.liferay.portlet.dynamicdatamapping.layout.FormPage;
+import com.liferay.portlet.dynamicdatamapping.layout.FormSection;
+import com.liferay.portlet.dynamicdatamapping.layout.SectionLayout;
+
+import java.util.List;
+
+public class FormToJSONConverter {
+	public String convert(Form layout) {
+		_stringBuilder = new StringBuilder();
+
+		_stringBuilder.append("{\"pages\":");
+		convert(layout.getPages());
+		_stringBuilder.append('}');
+
+		return _stringBuilder.toString();
+	}
+
+	protected <T> void convert(List<T> list) {
+		_stringBuilder.append('[');
+		boolean firstItem = true;
+		for (T item: list) {
+			if (!firstItem) _stringBuilder.append(',');
+			convert(item);
+		}
+		_stringBuilder.append(']');
+	}
+
+	protected void convert(FormPage page) {
+		_stringBuilder.append("{\"sections\":");
+		convert(page.getSections());
+		_stringBuilder.append('}');
+	}
+
+
+	protected void convert(FormSection section) {
+		_stringBuilder.append("{\"layout\":");
+		convert(section.getLayout());
+		_stringBuilder.append(",\"fields\":");
+		convert(section.getFields());
+		_stringBuilder.append('}');
+	}
+
+	protected void convert(SectionLayout sectionLayout) {
+		_stringBuilder.append("{}");
+	}
+
+	protected void convert(FormField field) {
+		_stringBuilder.append("{}");
+	}
+
+	protected void convert(Object obj) {
+		throw new IllegalArgumentException(
+			"Unrecognized type:" + obj.getClass().getName());
+	}
+
+	private StringBuilder _stringBuilder;
+}
---
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/storage/JSONToFormConverter.java
@@ -0,0 +1,105 @@
+package com.liferay.portlet.dynamicdatamapping.storage;
+
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portlet.dynamicdatamapping.layout.FormField;
+import com.liferay.portlet.dynamicdatamapping.layout.Form;
+import com.liferay.portlet.dynamicdatamapping.layout.FormPage;
+import com.liferay.portlet.dynamicdatamapping.layout.FormSection;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class JSONToFormConverter {
+	public JSONToFormConverter(String layoutJSON, String structureJSON)
+		throws JSONException {
+		this(layoutJSON, structureJSON, null);
+	}
+
+	public JSONToFormConverter(
+		String layoutJSON, String structureJSON, String valuesJSON) 
+		throws JSONException  {
+
+		_layoutRoot = JSONFactoryUtil.createJSONObject(layoutJSON);
+		_fieldsLayoutRoot = _layoutRoot.getJSONObject("fieldsLayout");
+
+		_structureRoot = JSONFactoryUtil.createJSONObject(structureJSON);
+
+		if (valuesJSON != null) {
+			_valuesRoot = JSONFactoryUtil.createJSONObject(valuesJSON);
+		}
+	}
+
+	public Form convert() {
+		Form form = new Form();
+		JSONArray pagesArray = _layoutRoot.getJSONArray("pages");
+		form.setPages(getPages(pagesArray));
+		return form;
+	}
+
+	protected List<FormPage> getPages(JSONArray pagesArray) {
+		List<FormPage> pages = new LinkedList<FormPage>();
+		for (int i = 0; i < pagesArray.length(); ++i) {
+			pages.add(getPage(pagesArray.getJSONObject(i)));
+		}
+		return pages;
+	}
+
+	protected FormPage getPage(JSONObject pageNode) {
+		FormPage page = new FormPage();
+		JSONArray sectionsArray = pageNode.getJSONArray("sections");
+		page.setSections(getSections(sectionsArray));
+		return page;
+	}
+
+	protected List<FormSection> getSections(JSONArray sectionsArray) {
+		List<FormSection> sections = new LinkedList<FormSection>();
+		for (int i = 0; i < sectionsArray.length(); ++i) {
+			sections.add(getSection(sectionsArray.getJSONObject(i)));
+		}
+		return sections;
+	}
+
+	protected FormSection getSection(JSONObject sectionNode) {
+		FormSection section = new FormSection();
+		JSONArray fieldsArray = sectionNode.getJSONArray("fields");
+		section.setFields(getFields(fieldsArray));
+		return section;
+	}
+
+	protected List<FormField> getFields(JSONArray fieldsArray) {
+		List<FormField> fields = new LinkedList<FormField>();
+		for (int i = 0; i < fieldsArray.length(); ++i) {
+			fields.add(getField(fieldsArray.getJSONObject(i)));
+		}
+		return fields;
+	}
+
+	protected FormField getField(JSONObject fieldNode) {
+		FormField formField = new FormField();
+
+		String fieldName = fieldNode.getString("name");
+		formField.setName(fieldName);
+
+		JSONObject fieldLayout = _fieldsLayoutRoot.getJSONObject(fieldName);
+		formField.setLabel(fieldLayout.getString("label"));
+		formField.setTip(fieldLayout.getString("tip"));
+
+		JSONObject fieldStructure = _structureRoot.getJSONObject(fieldName);
+		formField.setNestedFields(
+				getFields(fieldStructure.getJSONArray("nestedFields")));
+
+		if (_valuesRoot != null) {
+			JSONObject fieldValue = _valuesRoot.getJSONObject(fieldName);
+		}
+
+		return formField;
+	}
+
+	private JSONObject _layoutRoot;
+	private JSONObject _fieldsLayoutRoot;
+	private JSONObject _structureRoot;
+	private JSONObject _valuesRoot;
+}
