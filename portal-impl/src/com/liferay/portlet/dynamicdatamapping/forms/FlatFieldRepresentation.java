6e3f0839dd56450cfcb48ae2584e9d7cb2b324ec
Pablo Pinheiro <pablo.carvalho@liferay.com>
2014-04-22 15:37:15 -0300

    Changed FlatFieldRepresentation to store a list of values for each field

--- portal-impl/src/com/liferay/portlet/dynamicdatamapping/forms/FlatFieldRepresentation.java
+++ portal-impl/src/com/liferay/portlet/dynamicdatamapping/forms/FlatFieldRepresentation.java
@@ -1,25 +1,35 @@
 package com.liferay.portlet.dynamicdatamapping.forms;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 public class FlatFieldRepresentation {
 	public FlatFieldRepresentation() {
-		_fields = new HashMap<String, FormFieldValue>();
+		_fields = new HashMap<String, List<FormFieldValue> >();
 	}
 
-	public void addFieldValue(FormFieldValue field) {
-		_fields.put(field.getFieldName(), field);
+	public void addFieldValue(FormFieldValue fieldValue) {
+		List<FormFieldValue> currentValues = _fields.get(
+			fieldValue.getFieldName());
+
+		if (currentValues == null) {
+			currentValues = new ArrayList<FormFieldValue>();
+			_fields.put(fieldValue.getFieldName(), currentValues);
+		}
+
+		currentValues.add(fieldValue);
 	}
 
-	public void addFieldValue(Iterable<FormFieldValue> fields) {
-		for (FormFieldValue field : fields) {
-			addFieldValue(field);
+	public void addFieldValue(Iterable<FormFieldValue> fieldValues) {
+		for (FormFieldValue fieldValue : fieldValues) {
+			addFieldValue(fieldValue);
 		}
 	}
 
-	public FormFieldValue getFieldValue(String fieldName) {
+	public List<FormFieldValue> getFieldValue(String fieldName) {
 		return _fields.get(fieldName);
 	}
 
-	private Map<String, FormFieldValue> _fields;
+	private Map<String, List<FormFieldValue> > _fields;
 }
