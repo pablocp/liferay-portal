c6227364b35c55b69b67b5d057e4814dadd1a485
Pablo Pinheiro <pablo.carvalho@liferay.com>
2014-10-16 15:31:16 -0300

    LPS-50939 SF

--- portal-service/src/com/liferay/portlet/dynamicdatamapping/NoSuchStructureVersionException.java
+++ portal-service/src/com/liferay/portlet/dynamicdatamapping/NoSuchStructureVersionException.java
@@ -11,7 +11,8 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
-	package com.liferay.portlet.dynamicdatamapping;
+
+package com.liferay.portlet.dynamicdatamapping;
 
 import aQute.bnd.annotation.ProviderType;
 
